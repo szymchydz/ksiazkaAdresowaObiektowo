a6e49504c3f4af9192d1141f11da6f093df7eea9
Szymon Chydziński <szymon.chydzinski.programista@gmail.com>
2024-02-04 17:38:57 +0100

    Add registered user to file

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -12,16 +12,22 @@ class KsiazkaAdresowa {
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
 
+    string nazwaPlikuZUzytkownikami;
+
     vector <Uzytkownik> uzytkownicy;
 
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
     bool czyIstniejeLogin(string login);
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
+    string konwerjsaIntNaString(int liczba);
 
 public:
 
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wypiszUzytkownikow();
-
 
 };
